51b26a05334d3caf59e535ee0d8c973769299a2f
Eugene <jeni112211@gmail.com>
2022-12-09 05:34:57 +0600

    scss

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,48 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { RoutesEnum } from '../../const/routes';
+import Background from '../Background/Background';
 import './header.scss';
 
 export default function Header() {
     return <>
-        <div style={{
-            display: "flex",
-            alignItems: 'center',
-            maxWidth: '100%',
-            justifyContent: 'space-between',
-            background: '#020202',
-            padding: '1rem 0 1rem 0'
-        }}>
+        <div className='header'>
             <div>
-            <Link to={RoutesEnum.Home}><h1 style={{
-                    marginLeft: '3rem',
-                    color: '#ce0037'
-                }}>HDparadise</h1></Link>
-                
+                <Link to={RoutesEnum.Home}>
+                    <h1 className='logo'>HDparadise</h1>
+                </Link>
             </div>
             <menu>
-                <ul style={{
-                    display: "flex",
-                    alignItems: 'center',
-                    listStyle: 'none',
-                    gap: '3rem',
-                    color: '#ce0037',
-                    marginLeft: '5rem',
-                    marginRight: '3rem',
-                }}>
+                <ul className='menu-list'>
                     <Link to={RoutesEnum.Films}><li>Фильмы</li></Link>
                     <Link to={RoutesEnum.Serials}><li>Сериалы</li></Link>
                     <Link to={RoutesEnum.Cartoons}><li>Мультики</li></Link>
                 </ul>
             </menu>
-            <Link to={RoutesEnum.Registration} style={{
-                        width: 18,
-                        color: '#ce0037',
-                        marginLeft: '5rem',
-                        textDecoration: 'none'
-                    }}>
+            <Link to={RoutesEnum.Registration} className='registration'>
                     <span>Иконка авторизации</span>
             </Link>
         </div>
+        <Background />
     </>
 }
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -7,7 +7,6 @@ import Footer from '../components/Footer/Footer';
 const Layout = () => {
     return <> 
       <Header />
-      <Background />
       <List />
       <Footer />
     </>
