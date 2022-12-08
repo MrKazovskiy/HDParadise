eefa4539334b4cdfa2bf7fcc360425e102ff7c99
Eugene <jeni112211@gmail.com>
2022-12-09 04:58:51 +0600

    Add new type, const API for future and scss

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,40 +1,35 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
-
 function List() {
     const [list, setList] = useState<Object>({});
     console.log(list);
     
-     useEffect( () => {
-            try{
-            const res = axios.get('https://kinobd.ru/api/films', {
-                headers: {
-                    
-                    'Access-Control-Allow-Origin': '*',
-                    crossdomain: true,
-                },
-                proxy: {
-                    host: '5.45.64.97',
-                    port: 3128
-                }
+    const getFilms = async () => {
+        try{
+            const res = await axios.get('https://kinobd.ru/api/films', {
+                headers: {Accept: 'application/json', 'Content-Type': 'application/json',}
             })            
-                .then(res => {
-                    const film  = res.data.data;            
-                    setList(film);
-                    console.log(film);
-                })
-            }    
-            catch (error){
-                throw new Error(`Error ${error}`);
-                
-            }  
+            .then(res => {
+                const film  = res.data.data;            
+                setList(film);
+                console.log(film);
+            })
+        }    
+        catch (error){
+            throw new Error(`Error ${error}`);
+            
+        }  
+    }
+    
+     useEffect( () => {
+        getFilms();
     }, [])
     
     return <>
         {Array.isArray(list) ?
             list.map( (item) => <div key={item.id}>
-                <video width='500' height='500'>
+                <video width='150' height='300'>
                     <source src={item.trailer} type='video/mp4' />
                 </video>    
             </div>) : null
