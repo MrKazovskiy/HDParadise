246df7062992ab85eb21834922f94cddb74932f1
Eugene <jeni112211@gmail.com>
2022-12-09 23:12:00 +0600

    hide import

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { API_KEY, API_ID } from '../../const/api';
+// import { API_KEY, API_ID } from '../../const/api';
 import instance from '../../services/services';
 import './_list.scss';
 
