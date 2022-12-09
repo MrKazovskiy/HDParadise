15676e6037c07b1fe0933552ecce464a033e9f9f
Eugene <jeni112211@gmail.com>
2022-12-09 23:19:45 +0600

    edit

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-// import React, { useEffect, useState } from 'react';
-// import { API_KEY, API_ID } from '../../const/api';
-// import instance from '../../services/services';
+import React, { useEffect, useState } from 'react';
+import { API_KEY, API_ID } from '../../const/api';
+import instance from '../../services/services';
 import './_list.scss';
 
 function List() {
-    // const [films , setFilms] = useState<any>();
-    // console.log(films);
+    const [films , setFilms] = useState<any>();
+    console.log(films);
     
-    // const getID = async () => {
-    //     await instance.get(API_ID)
-    //             .then(res => {
-    //                 console.log(res);
-    //                 setFilms(res)
-    //                 return res
-    //             })
-    // }
+    const getID = async () => {
+        await instance.get(API_ID)
+                .then(res => {
+                    console.log(res);
+                    setFilms(res)
+                    return res
+                })
+    }
 
     // const dataList = async ()  => {
     //    const data = await instance.get(`/film/303`)
@@ -31,7 +30,7 @@ function List() {
     // }, [])
 
     return <>
-        <div className='container'>
+        <main className='container'>
             <div className='list__block'>
                 <h1>Смотрите кино бесплатно</h1>
                 {/* {films.map( (items: string | undefined, id: React.Key | null | undefined) => <div key={id}>
@@ -39,7 +38,7 @@ function List() {
                 </div> 
                 )} */}
             </div>
-        </div>
+        </main>
     </>
 }
 
