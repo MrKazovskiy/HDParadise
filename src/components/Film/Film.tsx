74104e7bbbfb8c5817f2c7ece50966112e18eb13
Eugene <jeni112211@gmail.com>
2022-12-18 08:52:07 +0600

    fix error

--- src/components/Film/Film.tsx
+++ src/components/Film/Film.tsx
@@ -4,7 +4,11 @@ import { API_KEY } from '../../const/api';
 import { RoutesEnum } from '../../const/routes';
 import { instance } from '../../services/services';
 import { IKinoMovie } from '../../types/IKinoMovie';
-
+import styles from './film.module.scss';
+import className from 'classnames';
+import { convertMovieType } from '../../helpers/convertMovieType/convertMovieType';
+import { convertNumbers } from '../../helpers/ConvertNumbers/convertNumbers';
+import { convertTimestampToDate } from '../../helpers/convertTimestampToDate/convertTimestampToDate';
 
 type FilmItemParams = {
   id: string | undefined;
@@ -12,7 +16,8 @@ type FilmItemParams = {
 
 export const Film: FC = () => {
     const [film , setFilm] = useState<IKinoMovie>();   
-
+    console.log(film);
+    
     const params = useParams<FilmItemParams>();
     const id = params.id
 
@@ -39,94 +44,117 @@ export const Film: FC = () => {
     }
 
     return <>
-      <main>
-          <button onClick={() => handler()}>На главную</button>
-          <section>
-            <div>
-              <img src={film?.poster.previewUrl} alt={film?.description} />
+      <main className={styles.main}>
+        <section className={styles.section}>
+          <div className={className('container wrapper', styles.container)}>
+            <div className={styles.top}>
+                <button 
+                  onClick={() => handler()}
+                  className={styles.button}
+                >
+                  На главную
+                </button>
             </div>
-            <div>
-            <h1>{film?.name}({film?.year})</h1>
-            <p>{film?.slogan}</p>
-            </div>
-            <div>
-              <button>Смотреть</button>
-              <button>Буду смотреть</button>
-            </div>
-            <div>
-              <h2>О фильме</h2>
-              <ul>
-                <li>
-                  <span>
-                    Премьера в мире
-                  </span>
-                  <span>
-                    {film?.year}
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Страна
-                  </span>
-                    {film?.countries.map((item, index) => <span key={index}>{item.name}</span>)}
-                </li>
-                <li>
-                  <span>
-                    Жанр
-                  </span>
-                    {film?.genres.map((item, index) => <span key={index}>{item.name}</span>)}
-                </li>
-                <li>
-                  <span>
-                    Слоган
-                  </span>
-                  <span>
+            <div className={styles.content}>
+              <div className={styles.left}>
+                <img
+                  src={film?.poster?.previewUrl}
+                  alt={film?.description ? film?.description : film?.shortDescription}
+                  className={styles.image}
+                />
+              </div>
+              <div
+                className={styles.right}
+              >
+                  <h1
+                    className={styles.title}
+                  >
+                    {film?.name ? film.name : film?.enName || film?.alternativeName} ({film?.year})
+                  </h1>
+                  <span
+                    className={styles.originalTitle}
+                  >
                     {film?.slogan}
                   </span>
-                </li>
-                <li>
-                  <span>
-                    Возраст
-                  </span>
-                  <span>
-                    {film?.ageRating}+
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Бюджет
-                  </span>
-                  <span>
-                    {film?.budget.currency} {film?.budget.value}
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Время
-                  </span>
-                  <span>
-                    {film?.movieLength} мин.
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Сборы в США
-                  </span>
-                  <span>
-                    {film?.year}
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    Сборы в мире
-                  </span>
-                  <span>
-                    {film?.year}
-                  </span>
-                </li>
-              </ul>
+                <div className={styles.btns}>
+                  <button className={styles.button}>
+                    Смотреть
+                  </button>
+                  <button className={styles.button}>
+                    Буду смотреть
+                  </button>
+                </div>
+                <div>
+                  <h2 className={styles.subtitle}>
+                    О {convertMovieType(film?.type)}е
+                  </h2>
+                  <ul className={styles.list_reset}>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Премьера в мире
+                      </span>
+                      <span className={styles.info_value}>
+                        {convertTimestampToDate(film?.premiere.world, 'D MMMM YYYY')}
+                      </span>
+                    </li>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Страны
+                      </span>
+                        {film?.countries.map((item, index) => <span  className={styles.info_value} key={index}>{index ? ', ' : ''}{item.name}</span>)}
+                    </li>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Жанр
+                      </span>
+                        {film?.genres.map((item, index) => <span  className={styles.info_value} key={index}>{index ? ', ' : ''}{item.name}</span>)}
+                    </li>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Возраст
+                      </span>
+                      <span className={styles.age}>
+                        {film?.ageRating ? film?.ageRating : 'Нет информации'}+
+                      </span>
+                    </li>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Бюджет
+                      </span>
+                      <span className={styles.info_value}>
+                        {film?.budget.currency ? film?.budget.currency : 'Нет информации'} {convertNumbers(film?.budget.value)}
+                      </span>
+                    </li>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Время
+                      </span>
+                      <span className={styles.info_value}>
+                        {film?.movieLength} мин.
+                      </span>
+                    </li>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Сборы в США
+                      </span>
+                      <span className={styles.info_value}>
+                        {film?.year}
+                      </span>
+                    </li>
+                    <li className={styles.info_item}>
+                      <span className={styles.info_caption}>
+                        Сборы в мире
+                      </span>
+                      <span className={styles.info_value}>
+                        {film?.year}
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </section>
+          </div>
+        </section>
       </main>
     </>
 }
